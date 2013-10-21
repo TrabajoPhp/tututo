b44dbd8812f7e27cd5836c489a420659dfc554d4
Alicia Rosenthal <aliciarosenthal@gmail.com>
2013-10-21 13:45:09 -0300

    levantando datos de la base

--- perfil.php
+++ perfil.php
@@ -1,50 +1,58 @@
 <?php require_once('header.php'); ?>
-		<div id="main">
-			<section>
-				<h1>Perfil de XXXXX</h1>
-				<div id="imgperfil">
-					<img src="img/perfil/perfil.jpg" alt="" />
-				</div>
-				<div id="datosperfil">
-					<ul>
-						<li>Nombre y Apellido: AAAAAA AAAAAAA<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
-						<li>Nickname: AAAAAA AAAAAAA<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
-						<li>Email: AAAAAA AAAAAAA<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
-						<li>Edad: AAAAAA AAAAAAA<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
-					</ul>
-				</div>
-				<div id="tutoriales">
-					<a href="crearTutorial.php" class="boton3">Crear Tutorial</a>
+	<?php require_once('conexion.php');
+		$registros=mysql_query("select * from usuario where nickname='$_GET[nickname]'",$con);
+		while($reg=mysql_fetch_array($registros))
+		{
+		echo('
+			<div id="main">
+				<section>
+					<h1>Perfil de '.$reg["nickname"].'</h1>
+					<div id="imgperfil">
+						<img src="img/perfil/perfil.jpg" alt="" />
+					</div>
+					<div id="datosperfil">
+						<ul>
+							<li>Nombre y Apellido: '.$reg["nombre"].'<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
+							<li>Nickname: '.$reg["nickname"].'<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
+							<li>Email: '.$reg["email"].'<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
+							<li>Edad: AAAAAA AAAAAAA<a href="#" class="borrar"></a><a href="#" class="editar"></a></li>
+						</ul>
+					</div>
+					<div id="tutoriales">
+						<a href="crearTutorial.php" class="boton3">Crear Tutorial</a>
+						<h3>
+							Tutoriales de '.$reg["nickname"].'
+							<a class="next" href="#"></a>
+							<a class="prev" href="#"></a>
+						</h3>
+						<ul>
+			');
+		}
+			$registro2=mysql_query("select * from tutorial INNER JOIN usuario ON(tutorial.autor=usuario.nickname) where nickname='$_GET[nickname]'",$con);
+			while($reg2=mysql_fetch_array($registro2))
+			{
+				echo('
+							<li>
+								<img src="../img/tutoriales/img_tut03.jpg" alt="" /><p>'.$reg2["titulo"].' - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php?id='.$reg2["id_tutorial"].'" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
+							</li>
+				');
+			}
+			?>
+						</ul>
+					</div>
 					<h3>
-						Tutoriales de xxxx
+						xx Items			
 						<a class="next" href="#"></a>
 						<a class="prev" href="#"></a>
+						<a class="pag" href="#">8</a>
+						<a class="pag" href="#">7</a>
+						<a class="pag" href="#">6</a>
+						<a class="pag" href="#">5</a>
+						<a class="pag" href="#">4</a>
+						<a class="pag" href="#">3</a>
+						<a class="pag" href="#">2</a>
+						<a class="selected" href="#">1</a>
 					</h3>
-					<ul>
-						<li>
-							<img src="../img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="../img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="../img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-					</ul>
-				</div>
-				<h3>
-					xx Items			
-					<a class="next" href="#"></a>
-					<a class="prev" href="#"></a>
-					<a class="pag" href="#">8</a>
-					<a class="pag" href="#">7</a>
-					<a class="pag" href="#">6</a>
-					<a class="pag" href="#">5</a>
-					<a class="pag" href="#">4</a>
-					<a class="pag" href="#">3</a>
-					<a class="pag" href="#">2</a>
-					<a class="selected" href="#">1</a>
-				</h3>
-			</section>
+				</section>
 			<?php require_once('lateral.php'); ?>
 		<?php require_once('footer.php'); ?>
--- tutoriales.php
+++ tutoriales.php
@@ -3,48 +3,29 @@
 			<section>
 				<div id="tutoriales">
 					<h3>
-						Tutoriales de xxxx
+						Tutoriales
 						<a class="next" href="#"></a>
 						<a class="prev" href="#"></a>
 					</h3>
 					<ul>
+					<?php require_once('conexion.php');
+					$registros=mysql_query("select * from tutorial INNER JOIN usuario ON(tutorial.autor=usuario.nickname)",$con);
+					while($reg=mysql_fetch_array($registros))
+					{
+						echo ('
 						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
+							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>'.$reg["titulo"].' - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php?id='.$reg["id_tutorial"].'" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
 						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
-						<li>
-							<img src="img/tutoriales/img_tut03.jpg" alt="" /><p>Titulo Tutorial - <a href="tutoriales.php">Categoría</a> - <a href="tutorial.php" class="boton4">Ver Tutorial</a><a href="#" class="borrar"></a><a href="#" class="editar"></a></p>
-						</li>
+						');
+					}?>
 					</ul>
 				</div>
 				<h3>
-					xx Items			
+					<?php
+					$registro2=mysql_query("select count(*) as items from tutorial",$con);
+					$reg2=mysql_fetch_array($registro2);
+					echo ($reg2["items"].' Items');
+					?>
 					<a class="next" href="#"></a>
 					<a class="prev" href="#"></a>
 					<a class="pag" href="#">8</a>
