6629a39f27bebc416c4d88d875a713d63a9ede70
Alicia Rosenthal <aliciarosenthal@gmail.com>
2013-11-05 01:26:14 -0300

    tutoriales en home

--- index.php
+++ index.php
@@ -11,36 +11,31 @@
 						<a class="next" href="#"></a>
 						<a class="prev" href="#"></a>
 					</h3>
-					<div class="tut">
-						<h4>Titulo Tutorial</h4>
-						<img src="img/tutoriales/img_tut.jpg" alt="" />
-						<h5><a href="perfil.php">Autor</a> | <a href="tutoriales.php">Categoría</a></h5>
-						<div class="rating">
-							<div class="izq"><input name="star1" type="radio" class="star"/> <input name="star1" type="radio" class="star"/> <input name="star1" type="radio" class="star"/> <input name="star1" type="radio" class="star"/> <input name="star1" type="radio" class="star"/> </div>
-							<div class="der"><a href="tutorial.php">Ver Tutorial</a></div>
+					<?php require_once('conexion.php');
+					$registros=mysql_query("select * from tutorial INNER JOIN usuario ON(tutorial.autor=usuario.nickname) order by fecha desc",$con);
+					$i=0;
+					while($reg=mysql_fetch_array($registros))
+					{
+						$i++;
+						echo ('
+						<div class="tut">
+							<h4>'.$reg["titulo"].'</h4>
+							<img src="img/tutoriales/img_tut.jpg" alt="" />
+							<h5><a href="perfil.php?nickname='.$reg["autor"].'">'.$reg["autor"].'</a> | <a href="tutoriales.php?categoria='.$reg["categoriatut"].'">'.$reg["categoriatut"].'</a></h5>
+							<div class="rating">
+								<div class="izq"><input name="star'.$i.'" type="radio" class="star"/> <input name="star'.$i.'" type="radio" class="star"/> <input name="star'.$i.'" type="radio" class="star"/> <input name="star'.$i.'" type="radio" class="star"/> <input name="star'.$i.'" type="radio" class="star"/> </div>
+								<div class="der"><a href="tutorial.php?id='.$reg["id_tutorial"].'">Ver Tutorial</a></div>
+							</div>
 						</div>
-					</div>
-					<div class="tut">
-						<h4>Titulo Tutorial</h4>
-						<img src="img/tutoriales/img_tut.jpg" alt="" />
-						<h5><a href="perfil.php">Autor</a> | <a href="tutoriales.php">Categoría</a></h5>
-						<div class="rating">
-							<div class="izq"><input name="star2" type="radio" class="star"/> <input name="star2" type="radio" class="star"/> <input name="star2" type="radio" class="star"/> <input name="star2" type="radio" class="star"/> <input name="star2" type="radio" class="star"/> </div>
-							<div class="der"><a href="tutorial.php">Ver Tutorial</a></div>
-						</div>
-					</div>
-					<div class="tut">
-						<h4>Titulo Tutorial</h4>
-						<img src="img/tutoriales/img_tut.jpg" alt="" />
-						<h5><a href="perfil.php">Autor</a> | <a href="tutoriales.php">Categoría</a></h5>
-						<div class="rating">
-							<div class="izq"><input name="star3" type="radio" class="star"/> <input name="star3" type="radio" class="star"/> <input name="star3" type="radio" class="star"/> <input name="star3" type="radio" class="star"/> <input name="star3" type="radio" class="star"/> </div>
-							<div class="der"><a href="tutorial.php">Ver Tutorial</a></div>
-						</div>
-					</div>
+						');
+					}?>
 				</div>
 				<h3>
-					xx Items			
+					<?php
+					$registro2=mysql_query("select count(*) as items from tutorial",$con);
+					$reg2=mysql_fetch_array($registro2);
+					echo ($reg2["items"].' Items');
+					?>		
 					<a class="next" href="#"></a>
 					<a class="prev" href="#"></a>
 					<a class="pag" href="#">8</a>
