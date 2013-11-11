e96e219fb911ef0221304f2d17d44278d864af8f
Alicia Rosenthal <aliciarosenthal@gmail.com>
2013-11-11 10:22:23 -0300

    base

--- subirtuto.php
+++ subirtuto.php
@@ -2,8 +2,8 @@
 session_start();
 include_once "conexion.php";
 		$fecha = date("Y-m-d H:i:s");
-		echo $fecha;
-		mysql_query("insert into tutorial (autor,titulo,contenido,tipotut,categoriatut,fecha)values('$_SESSION[usuarioactual]','$_REQUEST[titulo]','$_REQUEST[contenido]','$_REQUEST[tipo]','$_REQUEST[categoria]','$fecha')",$con) or die ("no puede hacer la consulta".mysql_error());
+		$contenido = mysql_real_escape_string ($_REQUEST['contenido']);
+		mysql_query("insert into tutorial (autor,titulo,contenido,tipotut,categoriatut,fecha)values('$_SESSION[usuarioactual]','$_REQUEST[titulo]','$contenido','$_REQUEST[tipo]','$_REQUEST[categoria]','$fecha')",$con) or die ("no puede hacer la consulta".mysql_error());
 		mysql_close($con) ;
 		echo 'Su tutorial se subio perfecto!';    
 ?>
